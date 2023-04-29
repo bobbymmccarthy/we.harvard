5326a4a2db6d88eb8c54fda50e425d76e765eeca
Ty Geri <tygeri@college.harvard.edu>
2023-04-29 16:33:52 -0400

    Added API endpoint for id search

--- server/routes/record.js
+++ server/routes/record.js
@@ -13,7 +13,6 @@ const ObjectId = require("mongodb").ObjectId;
  
  
 recordRoutes.route("/search").get(async function (req, res) {
-    console.log('hit API!')
     const agg = [
       {
         $search: {
@@ -41,63 +40,21 @@ recordRoutes.route("/search").get(async function (req, res) {
       res.status(500).send('An error occurred while processing the search request');
     }
   });
+
+
+  recordRoutes.route("/private_search").get(async function (req, res) {
+    const class_id = parseInt(req.query.id);
+    const coll = dbo.getDb("course_catalog").collection("myHarvard");
+    const query = { externalId: class_id };
+
+    try {
+        const result = await coll.findOne(query);
+        res.json(result)
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('An error occurred while processing the search request');
+    }
+  });
   
- 
-// // This section will help you get a single record by id
-// recordRoutes.route("/record/:id").get(function (req, res) {
-//  let db_connect = dbo.getDb();
-//  let myquery = { _id: ObjectId(req.params.id) };
-//  db_connect
-//    .collection("records")
-//    .findOne(myquery, function (err, result) {
-//      if (err) throw err;
-//      res.json(result);
-//    });
-// });
- 
-// // This section will help you create a new record.
-// recordRoutes.route("/record/add").post(function (req, response) {
-//  let db_connect = dbo.getDb();
-//  let myobj = {
-//    name: req.body.name,
-//    position: req.body.position,
-//    level: req.body.level,
-//  };
-//  db_connect.collection("records").insertOne(myobj, function (err, res) {
-//    if (err) throw err;
-//    response.json(res);
-//  });
-// });
- 
-// // This section will help you update a record by id.
-// recordRoutes.route("/update/:id").post(function (req, response) {
-//  let db_connect = dbo.getDb();
-//  let myquery = { _id: ObjectId(req.params.id) };
-//  let newvalues = {
-//    $set: {
-//      name: req.body.name,
-//      position: req.body.position,
-//      level: req.body.level,
-//    },
-//  };
-//  db_connect
-//    .collection("records")
-//    .updateOne(myquery, newvalues, function (err, res) {
-//      if (err) throw err;
-//      console.log("1 document updated");
-//      response.json(res);
-//    });
-// });
- 
-// // This section will help you delete a record
-// recordRoutes.route("/:id").delete((req, response) => {
-//  let db_connect = dbo.getDb();
-//  let myquery = { _id: ObjectId(req.params.id) };
-//  db_connect.collection("records").deleteOne(myquery, function (err, obj) {
-//    if (err) throw err;
-//    console.log("1 document deleted");
-//    response.json(obj);
-//  });
-// });
  
 module.exports = recordRoutes;
