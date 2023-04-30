97883b69a414d39be8e4461cfae00186cf9ef125
Ty Geri <tygeri@college.harvard.edu>
2023-04-30 10:49:41 -0400

    Adding subject filtering

--- server/routes/record.js
+++ server/routes/record.js
@@ -46,7 +46,6 @@ const ObjectId = require("mongodb").ObjectId;
 recordRoutes.route("/search").get(async function (req, res) {
     const gened = req.query.gened;
     const subject = req.query.gened;
-    console.log(gened);
     const agg = [
       {
         $search: {
@@ -55,6 +54,9 @@ recordRoutes.route("/search").get(async function (req, res) {
             query: req.query.query,
             path: {
               wildcard: "*"
+            },
+            fuzzy: {
+                maxEdits: 1
             }
           }
         }
@@ -73,9 +75,6 @@ recordRoutes.route("/search").get(async function (req, res) {
           }
         }
       ] : []),
-      {
-        $limit: 5
-      }
     ];
     const coll = dbo.getDb("course_catalog").collection("myHarvardExtended");
     let cursor = coll.aggregate(agg);
@@ -104,14 +103,31 @@ recordRoutes.route("/search").get(async function (req, res) {
   });
 
 
-  recordRoutes.route("/gened_filter").get(async function (req, res) {
+  recordRoutes.route("/gened").get(async function (req, res) {
     const gened_type = req.query.type;
-    const coll = dbo.getDb("course_catalog").collection("myHarvard");
-    const query = { externalId: class_id };
-
+    const coll = dbo.getDb("course_catalog").collection("myHarvardExtended");
+    const query = { GenedType: { $in: [gened_type] } };
+  
     try {
-        const result = await coll.findOne(query);
-        res.json(result)
+      const cursor = coll.find(query);
+      const results = await cursor.toArray();
+      res.json(results);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('An error occurred while processing the search request');
+    }
+  });
+
+  recordRoutes.route("/subject").get(async function (req, res) {
+    // const sub = req.query.type;
+    const sub = 'GERMAN'
+    const coll = dbo.getDb("course_catalog").collection("myHarvardExtended");
+    const query = { subject: sub };
+  
+    try {
+      const cursor = coll.find(query);
+      const results = await cursor.toArray();
+      res.json(results);
     } catch (err) {
       console.error(err);
       res.status(500).send('An error occurred while processing the search request');
