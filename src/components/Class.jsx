37c5ae8f39fe82baa5bacb7cf7b3a08e820043e4
Ty Geri <tygeri@college.harvard.edu>
2023-05-01 13:57:22 -0400

    Added instructors to class component

--- src/components/Class.jsx
+++ src/components/Class.jsx
@@ -82,6 +82,9 @@ export default function ClassCard() {
         {activeClass.subject} {activeClass.catalogNumber}: {activeClass.title}
       </Typography>
       <Typography sx={{ mb: 1.5 }} color="text.secondary">
+        {activeClass.instructors.map(instructor => instructor.name).join(", ")}
+      </Typography>
+      <Typography sx={{ mb: 1.5 }} color="text.secondary">
         {activeClass.semester} | {" "}
         {activeClass.meetingPatterns && activeClass.meetingPatterns[0] ? `${findAvail(activeClass.meetingPatterns).map((day) => weekdays[day]).join("")} ${convertTime(activeClass.meetingPatterns[0].startTime)}-${convertTime(activeClass.meetingPatterns[0].endTime)}` : "TBA"}
       </Typography>
